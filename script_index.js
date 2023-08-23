183448bfffe50b78f552a87c84b205ec429ba565
古昀玄 <kuyunxuan@Yun-Hsuans-MacBook-Pro.local>
2023-08-23 16:39:31 +0800

    update webpages

---
+++ script_index.js
@@ -0,0 +1,102 @@
+$(document).ready(function() {
+    $("#block_1").hover(
+        function() {
+            $(this).find(".text_menu").fadeOut(250); 
+            $(this).find(".text_menu2").fadeIn(250); 
+           
+        },
+        function() {
+            $(this).find(".text_menu2").fadeOut(250); 
+            $(this).find(".text_menu").fadeIn(250); 
+        }    
+    );
+});
+
+
+$(document).ready(function() {
+    $("#block_2").hover(
+        function() {
+            $(this).find(".text_menu").fadeOut(250); 
+            $(this).find(".text_menu2").fadeIn(250); 
+        },
+        function() {
+            $(this).find(".text_menu2").fadeOut(250); 
+            $(this).find(".text_menu").fadeIn(250); 
+        }
+    );
+});
+
+$(document).ready(function() {
+    $("#block_3").hover(
+        function() {
+            $(this).find(".text_menu").fadeOut(250); 
+            $(this).find(".text_menu2").fadeIn(250); 
+        },
+        function() {
+            $(this).find(".text_menu2").fadeOut(250); 
+            $(this).find(".text_menu").fadeIn(250); 
+        }
+    );
+});
+
+$(document).ready(function() {
+    $("#block_4").hover(
+        function() {
+            $(this).find(".text_menu").fadeOut(250); 
+            $(this).find(".text_menu2").fadeIn(250); 
+        },
+        function() {
+            $(this).find(".text_menu2").fadeOut(250); 
+            $(this).find(".text_menu").fadeIn(250); 
+        }
+    );
+});
+
+$(document).ready(function() {
+    $("#block_5").hover(
+        function() {
+            $(this).find(".text_menu").fadeOut(250); 
+            $(this).find(".text_menu2").fadeIn(250); 
+        },
+        function() {
+            $(this).find(".text_menu2").fadeOut(250); 
+            $(this).find(".text_menu").fadeIn(250); 
+        }
+    );
+});
+
+$(document).ready(function() {
+    $("#block_6").hover(
+        function() {
+            $(this).find(".text_menu").fadeOut(250); 
+            $(this).find(".text_menu2").fadeIn(250); 
+        },
+        function() {
+            $(this).find(".text_menu2").fadeOut(250); 
+            $(this).find(".text_menu").fadeIn(250); 
+        }
+    );
+});
+
+
+    $(document).ready(function() {
+        function toggleMenuFunctionality() {
+            if ($(window).width() <= 600) {
+                // 手機模式，使用點擊功能
+                $('.text_menu').on('click', function() {
+                    $(this).toggleClass('show_submenu');
+                });
+            } else {
+                // 電腦模式，使用 hover 功能
+                $('.text_menu').hover(function() {
+                    $(this).toggleClass('show_submenu');
+                });
+            }
+        }
+        
+        toggleMenuFunctionality();
+        
+        $(window).resize(function() {
+            toggleMenuFunctionality();
+        });
+    });
--- script_sts_about.js
+++ script_sts_about.js
@@ -1,7 +1,7 @@
 
 
 
-$(document).ready(function() {
+/*$(document).ready(function() {
     $('a[href^="#"]').on('click', function(event) {
         event.preventDefault();
         var target = $(this.getAttribute('href'));
@@ -11,10 +11,127 @@ $(document).ready(function() {
             }, 1300);
         }
     });
+});*/
+
+
+
+
+function menu1() {
+    var moreInfo = $("#menu_content1");
+    var moreBouton = $("#button_menu1");
+    if (moreInfo.is(":visible")) {
+        moreInfo.slideUp(1000);
+        moreBouton.css("background-color", "#00ADF1");
+        moreBouton.css("color", "#fff");
+
+    } else {
+        moreInfo.slideDown(1000);
+        moreInfo.css("display", "block");
+        moreBouton.css("background-color", "#fff");
+        moreBouton.css("color", "#00ADF1");
+
+    }
+}
+
+$(document).ready(function() {
+    $("#block_1").hover(
+        function() {
+            $(this).find(".text_menu").fadeOut(250); 
+            $(this).find(".text_menu2").fadeIn(250); 
+           
+        },
+        function() {
+            $(this).find(".text_menu2").fadeOut(250); 
+            $(this).find(".text_menu").fadeIn(250); 
+        }    
+    );
 });
 
 
+$(document).ready(function() {
+    $("#block_2").hover(
+        function() {
+            $(this).find(".text_menu").fadeOut(250); 
+            $(this).find(".text_menu2").fadeIn(250); 
+        },
+        function() {
+            $(this).find(".text_menu2").fadeOut(250); 
+            $(this).find(".text_menu").fadeIn(250); 
+        }
+    );
+});
+
+$(document).ready(function() {
+    $("#block_3").hover(
+        function() {
+            $(this).find(".text_menu").fadeOut(250); 
+            $(this).find(".text_menu2").fadeIn(250); 
+        },
+        function() {
+            $(this).find(".text_menu2").fadeOut(250); 
+            $(this).find(".text_menu").fadeIn(250); 
+        }
+    );
+});
+
+$(document).ready(function() {
+    $("#block_4").hover(
+        function() {
+            $(this).find(".text_menu").fadeOut(250); 
+            $(this).find(".text_menu2").fadeIn(250); 
+        },
+        function() {
+            $(this).find(".text_menu2").fadeOut(250); 
+            $(this).find(".text_menu").fadeIn(250); 
+        }
+    );
+});
+
+$(document).ready(function() {
+    $("#block_5").hover(
+        function() {
+            $(this).find(".text_menu").fadeOut(250); 
+            $(this).find(".text_menu2").fadeIn(250); 
+        },
+        function() {
+            $(this).find(".text_menu2").fadeOut(250); 
+            $(this).find(".text_menu").fadeIn(250); 
+        }
+    );
+});
+
+$(document).ready(function() {
+    $("#block_6").hover(
+        function() {
+            $(this).find(".text_menu").fadeOut(250); 
+            $(this).find(".text_menu2").fadeIn(250); 
+        },
+        function() {
+            $(this).find(".text_menu2").fadeOut(250); 
+            $(this).find(".text_menu").fadeIn(250); 
+        }
+    );
+});
 
 
-
-
+    $(document).ready(function() {
+        function toggleMenuFunctionality() {
+            if ($(window).width() <= 600) {
+                // 手機模式，使用點擊功能
+                $('.text_menu').on('click', function() {
+                    $(this).toggleClass('show_submenu');
+                });
+            } else {
+                // 電腦模式，使用 hover 功能
+                $('.text_menu').hover(function() {
+                    $(this).toggleClass('show_submenu');
+                });
+            }
+        }
+        
+        toggleMenuFunctionality();
+        
+        $(window).resize(function() {
+            toggleMenuFunctionality();
+        });
+    });
