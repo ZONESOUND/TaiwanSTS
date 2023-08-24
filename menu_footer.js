94361a525a6feb1d4c8a4c75d91974cfa8901cee
古昀玄 <kuyunxuan@Yun-Hsuans-MacBook-Pro.local>
2023-08-25 04:11:09 +0800

    update webpages

--- menu_footer.js
+++ menu_footer.js
@@ -1,4 +1,4 @@
-function menu1() {
+/*function menu1() {
     var moreInfo = $("#menu_content1");
     var moreBouton = $("#button_menu1");
     if (moreInfo.is(":visible")) {
@@ -8,23 +8,64 @@ function menu1() {
 
     } else {
         moreInfo.slideDown(1000);
-        moreInfo.css("display", "block");
+        moreInfo.css("display", "inline-grid");
         moreBouton.css("background-color", "#fff");
         moreBouton.css("color", "#00ADF1");
 
     }
+}*/
+
+function menu1() {
+    var moreInfo = $("#menu_content1");
+    var moreBouton = $("#button_menu1");
+
+    moreBouton.prop("disabled", true);
+
+    if (moreInfo.is(":visible")) {
+        moreInfo.stop(true, true).fadeOut(500, function() {
+            moreBouton.prop("disabled", false);
+        });
+        moreBouton.css("background-color", "#00ADF1");
+        moreBouton.css("color", "#fff");
+    } else {
+        moreInfo.stop(true, true).fadeIn(500, function() {
+            moreBouton.prop("disabled", false);
+        });
+        moreInfo.css("display", "inline-grid");
+        moreBouton.css("background-color", "#fff");
+        moreBouton.css("color", "#00ADF1");
+    }
+
+    if (window.innerWidth >= 600) {
+        moreInfo.css("display", "grid");
+    } else {
+        moreInfo.css("display", "block");
+    }
+    
+    moreBouton.css("background-color", "#fff");
+    moreBouton.css("color", "#00ADF1");
 }
+
+
+
+
+
 
 $(document).ready(function() {
     $("#block_1").hover(
         function() {
             $(this).find(".text_menu").fadeOut(250); 
             $(this).find(".text_menu2").fadeIn(250); 
-           
+            $("#block_2").css({ height: '25vw' });
+            $("#block_3").css({ height: '25vw' });
+            $("#block_1").css({ height: '25vw' });
         },
         function() {
             $(this).find(".text_menu2").fadeOut(250); 
             $(this).find(".text_menu").fadeIn(250); 
+            $("#block_2").css({ height: '15vw' });
+            $("#block_3").css({ height: '15vw' });
+            $("#block_1").css({ height: '15vw' });
         }    
     );
 });
@@ -35,11 +76,17 @@ $(document).ready(function() {
         function() {
             $(this).find(".text_menu").fadeOut(250); 
             $(this).find(".text_menu2").fadeIn(250); 
+            $("#block_2").css({ height: '25vw' });
+            $("#block_3").css({ height: '25vw' });
+            $("#block_1").css({ height: '25vw' });
+
         },
         function() {
             $(this).find(".text_menu2").fadeOut(250); 
             $(this).find(".text_menu").fadeIn(250); 
-        }
+            $("#block_2").css({ height: '15vw' });
+            $("#block_3").css({ height: '15vw' });
+            $("#block_1").css({ height: '15vw' });        }
     );
 });
 
@@ -48,10 +95,16 @@ $(document).ready(function() {
         function() {
             $(this).find(".text_menu").fadeOut(250); 
             $(this).find(".text_menu2").fadeIn(250); 
+            $("#block_2").css({ height: '25vw' });
+            $("#block_3").css({ height: '25vw' });
+            $("#block_1").css({ height: '25vw' });
         },
         function() {
             $(this).find(".text_menu2").fadeOut(250); 
             $(this).find(".text_menu").fadeIn(250); 
+            $("#block_2").css({ height: '15vw' });
+            $("#block_3").css({ height: '15vw' });
+            $("#block_1").css({ height: '15vw' });
         }
     );
 });
@@ -117,3 +170,5 @@ $(document).ready(function() {
             toggleMenuFunctionality();
         });
     });
+
+
